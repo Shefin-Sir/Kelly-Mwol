26cf5b449de084f27d5ba380747c921206d9f01d
Shefin-Sir <96101269+Shefin-Sir@users.noreply.github.com>
2021-12-14 11:39:48 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         });
 
         console.log(
-            chalk.green.bold('✅ Liza Mwol working!')
+            chalk.green.bold('✅ Kelly Mwol working!')
         );
         await new Promise(r => setTimeout(r, 1100));
 
@@ -396,8 +396,8 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '*~_________~ 𝐋𝐈𝐙𝐀 𝐌𝐖𝐎𝐋࿐ ~______~*' +
-                                    '\n\n*👾 ' + error + '*\n\n```Report errors\njoin ⚠️Warning bot not allowed in the group\nchat.whatsapp.com/FVOdpPLaMvP24rIfTzCGof``` ' 
+                                await conn.sendMessage(conn.user.jid, '*~_________~ 𝐊𝐄𝐋𝐋𝐘 𝐌𝐖𝐎𝐋࿐ ~______~*' +
+                                    '\n\n*👾 ' + error + '*\n\n```Report errors\njoin ⚠️Warning bot not allowed in the group\nchat.whatsapp.com/HtNneOrmY8W1U3gkP4pXOh``` ' 
                                     , MessageType.text);
                             }
                         }
